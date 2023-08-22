40d93ae86338a3fc053d622736e02162e191e772
Emma Brantley <brantled@mail.uc.edu>
2023-08-21 21:54:56 -0400

    win conditions

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,10 +12,13 @@ public class Game : MonoBehaviour
 
     public TextMeshProUGUI Instructions;
 
-    enum Seed { EMPTY, CROSS, NOUGHT };
+    public enum Seed { EMPTY, CROSS, NOUGHT };
 
     Seed Turn;
 
+    public GameObject[] allSpawns = new GameObject[9];
+
+    public Seed[] player = new Seed[9];
     private void Awake()
     {
         //get the game mode from the previous screen
@@ -25,23 +29,101 @@ public class Game : MonoBehaviour
         Turn = Seed.CROSS;
 
         Instructions.text = "Turn: Player 1";
+
+        for(int i = 0; i < 9; i++)
+        {
+            player[i] = Seed.EMPTY;
+        }
     }
 
     public void Spawn(GameObject emptycell, int id)
     {
         if (Turn == Seed.CROSS)
         {
-            Instantiate(cross, emptycell.transform.position, Quaternion.identity);
-            Turn = Seed.NOUGHT;
-            Instructions.text = "Turn: Player 2";
+            allSpawns[id] = Instantiate(cross, emptycell.transform.position, Quaternion.identity);
+            player[id] = Turn;
+
+            if (Won(Turn))
+            {
+                Turn = Seed.EMPTY;
+                Instructions.text = "Player 1 has won!";
+            }
+            else
+            {
+                Turn = Seed.NOUGHT;
+                Instructions.text = "Turn: Player 2";
+            }
         }
         else if (Turn == Seed.NOUGHT)
         {
-            Instantiate(nought, emptycell.transform.position, Quaternion.identity);
-            Turn = Seed.CROSS;
-            Instructions.text = "Turn: Player 1";
-        }
+            allSpawns[id] = Instantiate(nought, emptycell.transform.position, Quaternion.identity);
+            player[id] = Turn;
 
+            if (Won(Turn))
+            {
+                Turn = Seed.EMPTY;
+                Instructions.text = "Player 2 has won!";
+            }
+            else
+            {
+                Turn = Seed.CROSS;
+                Instructions.text = "Turn: Player 1";
+            }
+        }
+        if (IsDraw())
+        {
+            Turn = Seed.EMPTY;
+            Instructions.text = "It's a draw!";
+        }
         Destroy(emptycell);
     }
+
+    bool IsAnyEmpty()
+    {
+        bool empty = false;
+        for(int i = 0; i < 9; i++)
+        {
+            if(player[i] == Seed.EMPTY)
+            {
+                empty = true;
+                break;
+            }
+        }
+        return empty;
+    }
+
+    bool Won(Seed currPlayer)
+    {
+        bool hasWon = false;
+        int[,] allConditions = new int[8, 3] { {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+                                                {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+                                                {0, 4, 8}, {2, 4, 6} };
+
+        for (int i = 0; i < 8; i++)
+        {
+            if (player[allConditions[i, 0]] == currPlayer & player[allConditions[i,1]] == currPlayer & player[allConditions[i,2]] == currPlayer)
+            {
+                hasWon = true;
+                break;
+            }
+        }
+        return hasWon;
+    }
+    bool IsDraw()
+    {
+        bool player1Won, player2Won, anyEmpty;
+
+        player1Won = Won(Seed.CROSS);
+        player2Won = Won(Seed.NOUGHT);
+        anyEmpty = IsAnyEmpty();
+
+        bool isDraw = false;
+
+        if(player1Won == false & player2Won == false & anyEmpty == false)
+        {
+            isDraw = true;
+        }
+        return isDraw;
+    }
 }
+
